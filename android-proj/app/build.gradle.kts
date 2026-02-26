77633936201dfde72d0214ce004dbb1e3cb9da6f
jongwoo <izowooi@hotmail.com>
2026-02-26 23:12:47 +0900

    앱 네임 변경

--- android-proj/app/build.gradle.kts
+++ android-proj/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.izowooi.helltimer"
         minSdk = 30
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
