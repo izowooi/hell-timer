e46f7fbc3f0b653bb572c891c4a279e95db5c36f
jongwoo <izowooi@hotmail.com>
2026-03-06 07:27:02 +0900

    버전 코드 변경

--- android-proj/app/build.gradle.kts
+++ android-proj/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.izowooi.helltimer"
         minSdk = 30
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
